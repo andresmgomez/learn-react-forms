8c513e7963683eeefa3b7845cbb81ac38c33aaa6
Miguel Gomez <andresmgomez23@gmail.com>
2022-05-19 05:23:45 -0400

    Add an html5 form with what we are going to use in React

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 ## Learn some React Forms
+
+### Form
+
+1. What is a Form?
+   A form is a special html tag that lets you grab information
+
+2. What is an <input> tag
+   This is a tag inside a form that lets you react toward what the person wrote and how to handle this data
+
+3. What is an attribute in an <input> element?
+   A piece of text that looks like a <strong>prop</strong> that lets you change
+   the <em>behavior</em> how an input tag shows in
+
+   Ex: text:any string, number:any whole number, date:year,month,day, checkbox:either true or false, etc.
+
+Note: In our lesson yesterday we try to went every value inside the attribute <strong>type</strong>
--- src/ReactForm.js
+++ src/ReactForm.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function ReactForm() {
+	return (
+		<div className='App'>
+			<h1>Hi, I'm YourName This is my online form</h1>
+			{/* Scenario: Let's think you have to fill a form
+      the first time you are signing up to a College */}
+			<form method='post' className='App-header'>
+				<label htmlFor='firstName'>
+					First Name:
+					<input type='text' name='firstName' placeholder='Your First name' />
+				</label>
+				<br />
+				<label htmlFor='lastName'>
+					Last Name:
+					<input type='text' name='lastName' placeholder='Your Last name' />
+				</label>
+				<br />
+				<label htmlFor='address'>
+					Address:
+					<input
+						type='text'
+						name='address'
+						placeholder='What is your Address?'
+					/>
+				</label>
+				<br />
+				<label htmlFor='zipCode'>
+					Zip:
+					<input
+						type='number'
+						name='zipCode'
+						placeholder='What is your Zip code?'
+					/>
+				</label>
+				<br />
+				<label>
+					State:
+					<select name='nationState'>
+						<option selected='florida' value='florida'>
+							FL
+						</option>
+						<option value='north-carolina'>NC</option>
+						<option value='new-jersey'>NJ</option>
+						<option value='texas'>TX</option>
+					</select>
+				</label>
+				<br />
+				DOB:
+				<label htmlFor='birthDate'>
+					<input type='date' />
+				</label>
+				<br />
+				<br />
+				<label htmlFor='message'>
+					Your Message <br />
+					<textarea
+						name='message'
+						id='message'
+						cols='40'
+						rows='10'
+						placeholder='Any comments you would like to make...'
+					></textarea>
+				</label>
+				<br />
+				<label>
+					Agree with the <strong>Terms and Conditions</strong>
+					<input type='checkbox' name='agree' />
+				</label>
+				<br />
+				<button type='submit' disabled='false'>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
 
-export default App;
+export default ReactForm;
